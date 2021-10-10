a3aa8c6fc7981726b9802ff492f9484e0171d609
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2021-10-10 15:15:27 -0300

    insere inicio do exemplo de componenteComPropriedades

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import App2 from './App2';
+import ComponenteComPropriedades from './fundamentos/componenteComPropriedades';
 import { SafeAreaViewComponent, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <ComponenteComPropriedades/>
       <Text>Olá mundo!</Text>
       <StatusBar style="auto" />
       <App2/>
---
+++ src/fundamentos/ComponenteComPropriedades.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native'
+
+export default (param) => {
+    console.warn(param)
+    return (
+        <Text> 
+            Os valo2r {1 + 1} é maior que o valor y
+        </Text>
+    )
+}
+
+
