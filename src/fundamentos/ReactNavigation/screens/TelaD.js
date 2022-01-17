12c40a85004025eea1c7883510862afe4960cfac
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2022-01-17 08:38:13 -0300

    implementa navecacao do tipo stack

--- src/fundamentos/ReactNavigation/screens/TelaD.js
+++ src/fundamentos/ReactNavigation/screens/TelaD.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import TextoCentral from '../components/TextoCentral'
 
-export default props => (
-    <TextoCentral corFundo='pink'>
-        Tela D
-    </TextoCentral>
-)
+export default props => {
+
+    const route = props.route || {params: {numero: 0}}
+    
+    return(
+        <TextoCentral corFundo='pink'>
+            Tela D - {route.params.numero}
+        </TextoCentral>
+    )
+}
--- src/navegacao/Stack.js
+++ src/navegacao/Stack.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import TelaA from '../fundamentos/ReactNavigation/screens/TelaA'
+import TelaB from '../fundamentos/ReactNavigation/screens/TelaB'
+import TelaC from '../fundamentos/ReactNavigation/screens/TelaC'
+import TelaD from '../fundamentos/ReactNavigation/screens/TelaD'
+import PassoStack from '../fundamentos/ReactNavigation/components/PassoStack'
 
 const Stack = createNativeStackNavigator()
 
@@ -11,8 +15,44 @@ export default props => (
         >
         <Stack.Screen 
             name="TelaA" 
-            component={TelaA}
             options={{title: 'Muda o título do topo da tela'}}
-            />
+        >
+            {props => (
+                <PassoStack {...props} avancar="TelaB">
+                    <TelaA/>
+                </PassoStack>
+            )}
+        </Stack.Screen>
+        <Stack.Screen 
+            name="TelaB" 
+            options={{title: 'Muda o título do topo da tela 2'}}
+        >
+            {props => (
+                <PassoStack {...props} avancar="TelaC" voltar>
+                    <TelaB/>
+                </PassoStack>
+            )}
+        </Stack.Screen>
+        <Stack.Screen 
+            name="TelaC" 
+            options={{title: 'Muda o título do topo da tela 3'}}
+        >
+            {props => (
+                <PassoStack {...props} avancar="TelaD">
+                    <TelaC/>
+                </PassoStack>
+            )}
+        </Stack.Screen>
+        <Stack.Screen 
+            name="TelaD" 
+            options={{title: 'Muda o título do topo da tela 4'}}
+        >
+            {props => (
+                <PassoStack {...props} avancar="TelaD">
+                    <TelaD {...props}/>
+                </PassoStack>
+            )}
+        </Stack.Screen>
+
     </Stack.Navigator>
 )
